94809c11d1c05ab3baab370c2a868ccde8749689
Zaid Khandwani <khandwaniz@KECRPG.COM>
2023-09-06 20:06:29 +0530

    Initial

---
+++ CodeSculptor_UI/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSculptor_UI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        public static Component cmp = new Component();
+
+        [HttpPost]
+        public IActionResult SaveComponent(Component component)
+        {
+            cmp = component;
+            return Ok(true);
+        }
+
+        [HttpGet]
+        public IActionResult GetComponent()
+        {
+            return Ok(cmp);
+        }
+    }
+
+    public class Component
+    {
+        public string HTML { get; set; }
+        public string CSS { get; set; }
+        public List<string> ExternalCSSLinks { get; set; }
+    }
+}
